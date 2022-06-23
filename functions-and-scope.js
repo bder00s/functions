0a21a0f8b5ba936e2f5d522b648a1a69564e15dd
bder00s <bonne-deroos@hotmail.com>
2022-06-23 16:16:37 +0200

    uitwerkingen-1

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -15,7 +15,7 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
-
+err
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
